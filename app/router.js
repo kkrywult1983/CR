d1f285a444c1a5a86307256794b962b9ed390821
Kamil Krywult <cutmaster12@gmail.com>
2022-06-21 10:33:42 +0200

    added Bootstrap and scss

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('main', { path: '/' });
+
+  this.route('register');
+  this.route('login');
+});

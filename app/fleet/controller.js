8ea3dc8580ca190573618c9d1a729675085f4c7c
Kamil Krywult <cutmaster12@gmail.com>
2022-11-16 20:03:19 +0100

    test

--- app/fleet/controller.js
+++ app/fleet/controller.js
@@ -1,8 +1,8 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
-import moment from 'moment';
+// import { action } from '@ember/object';
+// import moment from 'moment';
 
 export default class FleetController extends Controller {
   @service store;

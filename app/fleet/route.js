b74143d3931dfef5bf59e4c9abfdf8d5c54d75f9
Kamil Krywult <cutmaster12@gmail.com>
2022-11-24 16:54:34 +0100

    Added Sorty by Car rent

--- app/fleet/route.js
+++ app/fleet/route.js
@@ -4,7 +4,10 @@ import { inject as service } from '@ember/service';
 export default class FleetRoute extends Route {
   @service store;
 
-  model() {
-    return this.store.findAll('car');
+  async model() {
+    // const { car_id } = params;
+    const data = await this.store.findAll('car');
+    // const carId = data.find(({ id }) => id === car_id);
+    return data;
   }
 }
